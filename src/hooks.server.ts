d8aeb3dea762d47e42dd76c575080bc48b234df2
Fabian Schwab <fabian.schwab@ibm.com>
2024-09-10 20:01:46 +0200

    Read theme from cookie and change theme html data tag for DaisyUI

---
+++ src/hooks.server.ts
@@ -0,0 +1,25 @@
+import type { Handle } from '@sveltejs/kit';
+import { sequence } from '@sveltejs/kit/hooks';
+import { availableThemes } from '$lib';
+
+/**
+ * Sets the theme from the cookie directly into the `html` tag, before returning it to the browser.
+ * This prevents flashing or flickering after a refresh or  re-visit of the page.
+ */
+const setThemeFromCookie: Handle = async ({ event, resolve }) => {
+	const themeFromCookie = event.cookies.get('theme');
+
+	console.log(themeFromCookie);
+
+	if (!themeFromCookie || !availableThemes.includes(themeFromCookie)) {
+		return await resolve(event);
+	}
+
+	return await resolve(event, {
+		transformPageChunk: ({ html }) => {
+			return html.replace('data-theme=""', `data-theme="${themeFromCookie}"`);
+		}
+	});
+};
+
+export const handle: Handle = sequence(setThemeFromCookie);
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,6 @@
 // place files you want to import through the `$lib` alias in this folder.
+/**
+ * Available themes for the page.
+ */
+const availableThemes = ['light', 'dark'];
+export { availableThemes };
