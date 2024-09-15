5d3f50c5e1cf1bd56cdb47223e961295f1328ee8
Fabian Schwab <fabian.schwab@ibm.com>
2024-09-15 12:41:48 +0200

    example kudo db schema

---
+++ src/lib/server/schema.ts
@@ -0,0 +1,12 @@
+import { sql } from 'drizzle-orm';
+import { integer, text, sqliteTable } from 'drizzle-orm/sqlite-core';
+
+export const Kudos = sqliteTable('kudos', {
+	id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }).notNull(),
+	from: text('from').notNull(),
+	to: text('to').notNull(),
+	message: text('message').notNull(),
+	date: text('date')
+		.default(sql`(CURRENT_TIMESTAMP)`)
+		.notNull()
+});
